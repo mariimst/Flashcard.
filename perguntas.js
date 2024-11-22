6a63d75143f0d95796c4978dd379fe2108a72a09
mariana <00001115010979sp@al.educacao.sp.gov.br>
2024-11-22 10:55:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    'Português', 
+    'Qual é a diferença "por que", "por quê"?',
+    'Resposta: 1 - Por que: Usado em perguntas diretas ou indiretas e significa "por qual razão" "ou por qual motivo". 2 - Por quê: Usado no final de frases onterrogativas e também  significa "por qual razão". Sempre vem acompanhado da pontuação (como interrogação).'
+)
+
+criaCartao(
+    'Matemática', 
+    'A probabilidade de, ao lançar dois dados de 6 faces, a soma ser igual a 7?',
+    'Resposta: 1/6.'
+)
+
+criaCartao(
+    'Educação fisica', 
+    'Em que ano o tênis de mesa foi fundado ?',
+    'Resposta: Ano de 1880'
+)
+
+criaCartao(
+    'História', 
+    'Quais foram as principais causas que levaram ao inicio da Segunda guerra mundial em 1939?',
+    'Resposta: O tratado de Versalhes (1919), a politica de apaziguamento das potências ocidentais, o imperialismo japonês no pacifico e sua aliança com a Alemanha e a Itália formaram as potências do eixo, a invasão da Polônia pela Alemanha em 1º de setembro de 1939.'
+)
+
+
+
